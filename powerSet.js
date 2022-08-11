09535d170a34b2000d2ae015ba6d4a3c78968bdf
JohnnyDamon <53366209+JohnnyDamon@users.noreply.github.com>
2022-08-11 21:36:24 +0900

    first commit

---
+++ powerSet.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+
+ var subsets = function(nums) {
+  const powerSet = [];
+  const dfs = (curr, rest, idx = 0) => {
+      powerSet.push(curr);
+      if (curr.length === nums.length) {
+          return;
+      }
+      for (let i = idx; i < rest.length; i++) {
+          dfs([...curr, rest[i]], [...rest.slice(0, i), ...rest.slice(i + 1)], i);
+      }
+  };
+  dfs([], nums);
+  return powerSet;
+};
